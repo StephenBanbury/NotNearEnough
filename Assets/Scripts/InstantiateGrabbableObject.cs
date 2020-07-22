b24603c54713d5d484e92b86bd9ad273df87d74d
Stephen Banbury <sbanbury@gmail.com>
2020-07-22 17:19:24 +0100

    Add another grabbable and request ownership on instantiate

--- Assets/Scripts/InstantiateGrabbableObject.cs
+++ Assets/Scripts/InstantiateGrabbableObject.cs
@@ -4,6 +4,7 @@ using Normal.Realtime;
 public class InstantiateGrabbableObject : MonoBehaviour
 {
     private Realtime _realtime;
+    private RealtimeTransform _realtimeTransform;
 
     private void Awake()
     {
@@ -11,12 +12,23 @@ public class InstantiateGrabbableObject : MonoBehaviour
         _realtime = GetComponent<Realtime>();
         // Notify us when Realtime successfully connects to the room
         _realtime.didConnectToRoom += DidConnectToRoom;
+
+        _realtimeTransform.RequestOwnership();
     }
 
     private void DidConnectToRoom(Realtime realtime)
     {
         //Instantiate the CubePlayer for this client once we've successfully connected to the room
-        Realtime.Instantiate("NormcoreGrabbable",                 // Prefab name
+        Realtime.Instantiate("NormcoreGrabbableCube",                 // Prefab name
+            position: Vector3.up,          // Start 1 meter in the air
+            rotation: Quaternion.identity, // No rotation
+            ownedByClient: false,   // Make sure the RealtimeView on this prefab is NOT owned by this client
+            preventOwnershipTakeover: false,                // DO NOT prevent other clients from calling RequestOwnership() on the root RealtimeView.
+            useInstance: realtime);           // Use the instance of Realtime that fired the didConnectToRoom event.
+
+
+        //Instantiate the CubePlayer for this client once we've successfully connected to the room
+        Realtime.Instantiate("NormcoreGrabbableSphere",                 // Prefab name
             position: Vector3.up,          // Start 1 meter in the air
             rotation: Quaternion.identity, // No rotation
             ownedByClient: false,   // Make sure the RealtimeView on this prefab is NOT owned by this client
