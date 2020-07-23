6d4482de010b689473229f5ca5975415bcb6b176
Stephen Banbury <sbanbury@gmail.com>
2020-07-23 23:04:27 +0100

    Detect and display hand position

---
+++ Assets/Scripts/BoxEntered.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoxEntered : MonoBehaviour
+{
+    [SerializeField] private Text _scaleText;
+
+    private float _handHeight;
+
+    public float HandHeight { 
+        get => _handHeight; 
+        set => _handHeight = value;
+    }
+
+    private void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Hand"))
+        {
+            Debug.Log("Hand entered!");
+            Debug.Log($"Extra: { other.transform}");
+
+            //_handHeight = other.transform.position.y;
+            //Debug.Log($"_handHeight: {_handHeight}");
+        }
+    }
+
+    // OnTriggerStay is called once per frame for every Collider other that is touching the trigger
+    private void OnTriggerStay(Collider other)
+    {
+        _handHeight = other.transform.position.y;
+        Debug.Log($"_handHeight: {_handHeight}");
+        _scaleText.text = _handHeight.ToString();
+    }
+
+
+}
