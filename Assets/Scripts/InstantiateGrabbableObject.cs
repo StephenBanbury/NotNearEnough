6184f224df29f5c63caba86326692e25c040372a
Stephen Banbury <sbanbury@gmail.com>
2020-08-19 17:31:34 +0100

    Lighting changes

--- Assets/Scripts/InstantiateGrabbableObject.cs
+++ Assets/Scripts/InstantiateGrabbableObject.cs
@@ -21,15 +21,6 @@ namespace Assets.Scripts
         private void DidConnectToRoom(Realtime realtime)
         {
             //Instantiate the CubePlayer for this client once we've successfully connected to the room
-            Realtime.Instantiate("NormcoreGrabbableCube", // Prefab name
-                position: Vector3.up, // Start 1 meter in the air
-                rotation: Quaternion.identity, // No rotation
-                ownedByClient: false, // Make sure the RealtimeView on this prefab is NOT owned by this client
-                preventOwnershipTakeover: false, // DO NOT prevent other clients from calling RequestOwnership() on the root RealtimeView.
-                useInstance: realtime); // Use the instance of Realtime that fired the didConnectToRoom event.
-
-
-            //Instantiate the CubePlayer for this client once we've successfully connected to the room
             Realtime.Instantiate("NormcoreGrabbableSphere", // Prefab name
                 position: Vector3.up, // Start 1 meter in the air
                 rotation: Quaternion.identity, // No rotation
