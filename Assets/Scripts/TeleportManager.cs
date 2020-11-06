e455cf0b8a9c3a780bebc6e08b5a4d49f964aa95
Stephen Banbury <sbanbury@gmail.com>
2020-11-06 15:01:33 +0000

    Teleportation works

--- Assets/Scripts/TeleportManager.cs
+++ Assets/Scripts/TeleportManager.cs
@@ -9,15 +9,24 @@ namespace Assets.Scripts
         {
             if (other.CompareTag("Hand"))
             {
+                Vector3 newPos = new Vector3(0f, 1f, 0);
+
                 var spawnPoint = GameObject.Find("SpawnPoint1").transform;
                 var capsule = GameObject.Find("Capsule").transform;
                 var player = GameObject.Find("Player").transform;
 
-                capsule.position = spawnPoint.position;
+                //capsule.position = spawnPoint.position;
+
                 player.position = spawnPoint.position;
+
+                //player.position = player.position + newPos;
 
                 //player.SetParent(spawnPoint);
                 //player.position = new Vector3(0f, 0f, 0f);
+
+                
+
+
             }
         }
 
