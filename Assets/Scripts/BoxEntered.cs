bfd9c5bdcb149fc12c00ed31781d7133c9acfe88
Stephen Banbury <sbanbury@gmail.com>
2020-11-07 12:14:47 +0000

    Teleportation works!

--- Assets/Scripts/BoxEntered.cs
+++ Assets/Scripts/BoxEntered.cs
@@ -39,23 +39,23 @@ namespace Assets.Scripts
         // OnTriggerStay is called once per frame for every Collider other that is touching the trigger
         private void OnTriggerStay(Collider other)
         {
-            if (other.CompareTag("Hand"))
-            {
-                // Round to nearest 2 decimal places
-                _scaleValue = Mathf.Round(other.transform.position.y * 100f) / 10f;
-                //Debug.Log($"_handHeight: {_scaleValue}");
-                _scaleText.text = _scaleValue.ToString();
+            //if (other.CompareTag("Hand"))
+            //{
+            //    // Round to nearest 2 decimal places
+            //    _scaleValue = Mathf.Round(other.transform.position.y * 100f) / 10f;
+            //    //Debug.Log($"_handHeight: {_scaleValue}");
+            //    _scaleText.text = _scaleValue.ToString();
 
 
-                // If the value has changed, call SetValue on the sliding scale sync component
-                if (_scaleValue != _previousScaleValue)
-                {
-                    //Debug.Log($"_scaleValue: {_scaleValue}, _previousScaleValue: {_previousScaleValue}");
+            //    // If the value has changed, call SetValue on the sliding scale sync component
+            //    if (_scaleValue != _previousScaleValue)
+            //    {
+            //        //Debug.Log($"_scaleValue: {_scaleValue}, _previousScaleValue: {_previousScaleValue}");
 
-                    _slidingScaleSync.SetValue(_scaleValue);
-                    _previousScaleValue = _scaleValue;
-                }
-            }
+            //        _slidingScaleSync.SetValue(_scaleValue);
+            //        _previousScaleValue = _scaleValue;
+            //    }
+            //}
         }
     }
 }
--- Assets/Scripts/TeleportManager.cs
+++ Assets/Scripts/TeleportManager.cs
@@ -1,26 +1,44 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
     public class TeleportManager : MonoBehaviour
     {
+        [SerializeField] private int _sceneId;
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Hand"))
             {
-                var spawnPoint = GameObject.Find("SpawnPoint1").transform;
-                var capsule = GameObject.Find("Capsule").transform;
-                var player = GameObject.Find("Player").transform;
-
-                capsule.position = spawnPoint.position;
-                player.position = spawnPoint.position;
-
-                //player.SetParent(spawnPoint);
-                //player.position = new Vector3(0f, 0f, 0f);
+                StartCoroutine(DoTeleportation());
             }
         }
 
-    }
+        private IEnumerator DoTeleportation()
+        {
+            string spawnPointName = $"Spawn Point {_sceneId}";
+            Transform spawnPoint = GameObject.Find(spawnPointName).transform;
+            Transform playerContainer = GameObject.Find("PlayerAudience").transform;
+            Transform player = playerContainer.Find("Player").transform;
+            var playerController = player.GetComponent<OVRPlayerController>();
+            var sceneSampleController = player.GetComponent<OVRSceneSampleController>();
 
+            //Debug.Log($"Teleporting to {spawnPointName}");
+            //Debug.Log($"SpawnPoint position: {spawnPoint.position}");
+
+            playerController.enabled = false;
+            sceneSampleController.enabled = false;
+
+            yield return new WaitForSeconds(0.5f);
+
+            playerContainer.position = spawnPoint.position;
+
+            yield return new WaitForSeconds(0.5f);
+
+            playerController.enabled = true;
+            sceneSampleController.enabled = true;
+        }
+    }
+    
 }
